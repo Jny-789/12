e5ebafa9e77e726646d7f9cdfb397b8e62e870c4
Jny-789 <953543899@qq.com>
2020-10-09 14:10:22 +0800

    again

--- erzhihua/test_2.cpp
+++ erzhihua/test_2.cpp
@@ -1,19 +1,19 @@
-//#include<iostream>
-//#include<opencv2/opencv.hpp>
-//
-//using namespace cv;
-//using namespace std;
-//
-//int main() {
-//	Mat srcMat, result_1,result_2;
-//	srcMat=imread("C://Users//1234//Desktop//pic.png",0);   //灰度化
-//	threshold(srcMat, result_1, 100, 255, THRESH_BINARY);
-//
-//	adaptiveThreshold(srcMat, result_2, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV,15,10);
-//
-//
-//	//imshow("result", result_1);
-//	imshow("result", result_2);
-//	waitKey(0);
-//	return 0;
-//}
+#include<iostream>
+#include<opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+	Mat srcMat, result_1,result_2;
+	srcMat=imread("C://Users//1234//Desktop//pic.png",0);   //灰度化
+	threshold(srcMat, result_1, 100, 255, THRESH_BINARY);
+
+	adaptiveThreshold(srcMat, result_2, 255, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY_INV,15,10);
+
+
+	//imshow("result", result_1);
+	imshow("result", result_2);
+	waitKey(0);
+	return 0;
+}
