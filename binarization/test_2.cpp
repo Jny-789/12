d4dcc87f2a46a3cab2999b54038fae47f1d7ac1b
Jny-789 <953543899@qq.com>
2020-10-16 15:17:10 +0800

    练习2

--- binarization/test_2.cpp
+++ binarization/test_2.cpp
@@ -1,29 +1,36 @@
-//#include<opencv2/opencv.hpp>
-//#include<iostream>
-//using namespace cv;
-//using namespace std;
-//
-//int main() {
-//	cv::Mat src, img, dst, stats, centroids;
-//	src = imread("C://Users//1234//Desktop//1.jpg");
-//	cv::threshold(src, img, 100, 255, THRESH_BINARY);      //二值化
-//
-//	imshow("img", img);
-//
-//	CV_EXPORTS_W int connectedComponentsWithStats(cv::Mat img , cv::Mat dst, OutputArray stats,OutputArray centroids,
-//		                           int connectivity= 8, int ltype=CV_32S);       //连通域标记
-//
-//	imshow("dst", dst);
-//	waitKey(0);
-//	return 0;
-//
-//	////矩形
-//	//Mat dispMat_rect;
-//	//cv::Rect rect;
-//	//dispMat_rect.create(500, 500, CV_8UC3);
-//	//rect.x = 150;
-//	//rect.y = 150;
-//	//rect.width = 200;
-//	//rect.height = 100;
-//	//rectangle(dispMat_rect, rect, CV_RGB(255, 0, 0, ), 1, 8, 0);
-//}
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace cv;
+using namespace std;
+
+int main() {
+	Mat src, img, labels, stats, centroids;
+	src = imread("C://Users//1234//Desktop//1.jpg",0);
+	threshold(src, img, 100, 255, THRESH_BINARY);      //二值化
+
+	int n = connectedComponentsWithStats(img, labels, stats, centroids, 8, CV_32S);      //连通域标记
+
+	cout << "n=" << n << endl;                     //连通域总数
+
+	for (int i = 0; i < n; i++) {
+		cout << "x=" << stats.at<int>(i,0) << endl;
+		cout << "y=" << stats.at<int>(i,1) << endl;
+		cout << "width=" << stats.at<int>(i,2) << endl;
+		cout << "height=" << stats.at<int>(i,3) << endl;
+		cout << "s=" << stats.at<int>(i,4) << endl;
+	}
+
+	//画矩形
+	cv::Rect rect;
+	for (int i = 1; i < n ; i++) {
+		rect.x = stats.at<int>(i, 0);
+		rect.y = stats.at<int>(i, 1);
+		rect.width = stats.at<int>(i, 2);
+		rect.height = stats.at<int>(1, 3);
+		rectangle(img, rect, CV_RGB(255, 255, 255, ), 1, 8, 0);
+	}
+
+	imshow("img", img);
+	waitKey(0);
+	return 0;
+}
