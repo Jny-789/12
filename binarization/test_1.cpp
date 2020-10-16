2f168515c120a83872534e012c635d394222d009
Jny-789 <953543899@qq.com>
2020-10-16 14:26:41 +0800

    练习1

--- binarization/test_1.cpp
+++ binarization/test_1.cpp
@@ -4,18 +4,17 @@ using namespace cv;
 using namespace std;
 
 int main() {
-	Mat src,result_1,dst_1,dst_2,dst_3,dst_4;
+	Mat src,img,dst_1,dst_2,dst_3,dst_4;
 	src = imread("C://Users//1234//Desktop//1.jpg");
-	threshold(src, result_1, 100, 255, THRESH_BINARY);    //二值化
+	threshold(src, img, 100, 255, THRESH_BINARY);    //二值化
 	Mat getStructuringElement(int shape, Size esize, Point anchor = Point(-1, -1));
 	int g_nStructElementSize = 3;           //结构元素(内核矩阵)的尺寸
 	Mat element = getStructuringElement(MORPH_RECT,Size(g_nStructElementSize, g_nStructElementSize));
 
-	erode(result_1, dst_1, element);               //腐蚀
-	dilate(result_1, dst_2, element);                //膨胀
-
-	morphologyEx(result_1, dst_3, MORPH_OPEN,element);  //开运算
-	morphologyEx(result_1, dst_4, MORPH_CLOSE, element);   //闭运算
+	erode(img, dst_1, element);               //腐蚀
+	dilate(img, dst_2, element);                //膨胀
+	morphologyEx(img, dst_3, MORPH_OPEN,element);  //开运算
+	morphologyEx(img, dst_4, MORPH_CLOSE, element);   //闭运算
 
 	imshow("腐蚀", dst_1);
 	imshow("膨胀", dst_2);
--- binarization/test_2.cpp
+++ binarization/test_2.cpp
@@ -1,26 +1,29 @@
-#include<opencv2/opencv.hpp>
-#include<iostream>
-using namespace cv;
-using namespace std;
-
-int main() {
-	Mat src,img, dst;
-	src = imread("C://Users//1234//Desktop//1.jpg");
-	threshold(src, img, 100, 255, THRESH_BINARY);    //二值化
-
-	CV_EXPORTS_W int connectedComponentsWithStats(Mat img, Mat dst, OutputArray stats, 
-		         OutputArray centroids,int connectivity = 8, int ltype = CV_32S);       //连通域标记
-
-
-	////矩形
-	//Mat dispMat_rect;
-	//cv::Rect rect;
-	//dispMat_rect.create(500, 500, CV_8UC3);
-	//rect.x = 150;
-	//rect.y = 150;
-	//rect.width = 200;
-	//rect.height = 100;
-	//rectangle(dispMat_rect, rect, CV_RGB(255, 0, 0, ), 1, 8, 0);
-
-
-}
+//#include<opencv2/opencv.hpp>
+//#include<iostream>
+//using namespace cv;
+//using namespace std;
+//
+//int main() {
+//	cv::Mat src, img, dst, stats, centroids;
+//	src = imread("C://Users//1234//Desktop//1.jpg");
+//	cv::threshold(src, img, 100, 255, THRESH_BINARY);      //二值化
+//
+//	imshow("img", img);
+//
+//	CV_EXPORTS_W int connectedComponentsWithStats(cv::Mat img , cv::Mat dst, OutputArray stats,OutputArray centroids,
+//		                           int connectivity= 8, int ltype=CV_32S);       //连通域标记
+//
+//	imshow("dst", dst);
+//	waitKey(0);
+//	return 0;
+//
+//	////矩形
+//	//Mat dispMat_rect;
+//	//cv::Rect rect;
+//	//dispMat_rect.create(500, 500, CV_8UC3);
+//	//rect.x = 150;
+//	//rect.y = 150;
+//	//rect.width = 200;
+//	//rect.height = 100;
+//	//rectangle(dispMat_rect, rect, CV_RGB(255, 0, 0, ), 1, 8, 0);
+//}
