f5d1026b5d783b76c72ae70670430034dc245099
Jny-789 <953543899@qq.com>
2020-10-30 20:03:59 +0800

    修改

--- juanji/test.cpp
+++ juanji/test.cpp
@@ -9,20 +9,24 @@ int main()
 
 	cap.open(0);
 
-	if (!cap.isOpened()) {
+	if (!cap.isOpened()) 
+	{
 		std::cout << "不能打开视频文件" << std::endl;
 		return -1;
 	}
 	double fps = cap.get(CAP_PROP_FPS);
 	std::cout << "fps" << fps << std::endl;
-	while (1) {
+	while (1) 
+	{
 		cv::Mat frame, dst_1, dst_2, dst_3, dst_4, dx, dy, dst_5;
 		bool rSucess = cap.read(frame);
-		if (!rSucess) {
+		if (!rSucess)
+		{
 			std::cout << "不能从视频文件中读取帧" << std::endl;
 			break;
 		}
-		else {
+		else 
+		{
 			cv::imshow("frame", frame);
 			//中值滤波
 			medianBlur(frame, dst_1, 5);
