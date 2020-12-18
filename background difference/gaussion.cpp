490e57c1a1d5af9d5febe81b4b21e3d7f2b8cc51
Jny-789 <953543899@qq.com>
2020-12-18 18:56:03 +0800

    课堂练习12

---
+++ background difference/gaussion.cpp
@@ -0,0 +1,76 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace cv;
+using namespace std;
+
+int main() {
+	VideoCapture cap;
+	cap.open(0);
+
+	if (!cap.isOpened()) {
+		std::cout << "打开视频失败" << std::endl;
+		return -1;
+	}
+	double fps = cap.get(CAP_PROP_FPS);
+
+	cv::Mat frame, bgMat, subMat, averMat, varMat;
+	
+	bool rSucess = cap.read(frame);
+	int width = frame.cols;
+	int height = frame.rows;
+	if (!rSucess) {
+		std::cout << "读取帧失败" << std::endl;
+		return -1;
+	}
+	else {
+		std::vector<Mat> bgMats;
+		int count = 0;
+		int sum = 0;
+		int var = 0;
+
+		while (1) {
+			cap >> frame;
+			cvtColor(frame, frame, COLOR_BGR2GRAY);
+
+			if (count == 0) {
+				frame.copyTo(bgMat);        //将第一帧作为背景
+				frame.copyTo(averMat);        //初始化，否则会报错
+				frame.copyTo(varMat);
+			}
+			else if(count<20){              //将20帧图像存入bgMats中，只计算20帧
+				bgMats.push_back(frame);
+			}
+			count++;
+			if(count==20){
+				/*********求均值、方差*********/
+				for (int i = 0; i < height; i++) {
+					for (int j = 0; j < width; j++) {
+						for (int n = 0; n < bgMats.size(); n++) {
+							sum += bgMats[n].at<uchar>(i, j); 	
+						}
+						averMat.at<uchar>(i,j) = sum / bgMats.size();      //均值
+						for (int n = 0; n < bgMats.size(); n++) {
+							var += pow(bgMats[n].at<uchar>(i, j) - averMat.at<uchar>(i, j), 2);
+						}
+						varMat.at<uchar>(i, j) = var / bgMats.size();      //方差
+					}
+				}
+				
+				/**********求均值、方差***********/
+
+				for (int i = 0; i < height; i++) {
+					for (int j = 0; j < width; j++) {					
+						if (abs(frame.at<uchar>(i, j) - averMat.at<uchar>(i, j)) > 13 * varMat.at<uchar>(i, j)) {					
+							frame.at<uchar>(i, j) = 255;
+					    }
+					    else frame.at<uchar>(i, j) = 0;
+				    }
+			    }
+
+				imshow("frame", frame);
+			    waitKey(30);
+		    }			
+		}			
+	}
+	return 0;
+}
