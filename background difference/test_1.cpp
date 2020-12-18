d65df62005c7de0c27053542845bf56e001d21db
Jny-789 <953543899@qq.com>
2020-12-18 18:56:59 +0800

    课堂练习12

--- background difference/test_1.cpp
+++ background difference/test_1.cpp
@@ -1,47 +1,47 @@
-//#include<opencv2/opencv.hpp>
-//#include<iostream>
-//using namespace cv;
-//using namespace std;
-//
-//int main() {
-//	VideoCapture cap;
-//
-//	cap.open(0);
-//
-//	if (!cap.isOpened()) {
-//		std::cout << "不能打开视频文件" << std::endl;
-//		return -1;
-//	}
-//	double fps = cap.get(CAP_PROP_FPS);      //获取视频帧率
-//
-//	Mat bgMat, subMat, bnyMat;
-//	while (1) {
-//		cv::Mat frame;
-//		bool rSucess = cap.read(frame);
-//		if (!rSucess) {
-//			std::cout << "不能从视频文件中读取帧" << std::endl;
-//			break;
-//		}
-//		else {
-//			int count = 0;
-//			while (1) {
-//				cap >> frame;
-//				cvtColor(frame, frame, COLOR_BGR2GRAY);
-//
-//				if (count == 0) {
-//					frame.copyTo(bgMat);        //将第一帧作为背景
-//				}
-//				else {
-//					absdiff(frame, bgMat, subMat);
-//					threshold(subMat, bnyMat, 100, 255, CV_THRESH_BINARY);    //二值化
-//
-//					imshow("frame", frame);
-//					imshow("bnyMat", bnyMat);
-//					waitKey(30);
-//				}
-//				count++;
-//			}
-//		}
-//	}
-//	return 0;
-//}
+#include<opencv2/opencv.hpp>
+#include<iostream>
+using namespace cv;
+using namespace std;
+
+int main() {
+	VideoCapture cap;
+
+	cap.open(0);
+
+	if (!cap.isOpened()) {
+		std::cout << "不能打开视频文件" << std::endl;
+		return -1;
+	}
+	double fps = cap.get(CAP_PROP_FPS);      //获取视频帧率
+
+	Mat bgMat, subMat, bnyMat;
+	while (1) {
+		cv::Mat frame;
+		bool rSucess = cap.read(frame);
+		if (!rSucess) {
+			std::cout << "不能从视频文件中读取帧" << std::endl;
+			break;
+		}
+		else {
+			int count = 0;
+			while (1) {
+				cap >> frame;
+				cvtColor(frame, frame, COLOR_BGR2GRAY);
+
+				if (count == 0) {
+					frame.copyTo(bgMat);        //将第一帧作为背景
+				}
+				else {
+					absdiff(frame, bgMat, subMat);
+					threshold(subMat, bnyMat, 100, 255, CV_THRESH_BINARY);    //二值化
+
+					imshow("frame", frame);
+					imshow("bnyMat", bnyMat);
+					waitKey(30);
+				}
+				count++;
+			}
+		}
+	}
+	return 0;
+}
